ad7de1be60ab5e0a0268fc8b880e0a16386130a2
Alexander Lalejini <amlalejini@gmail.com>
2025-08-11 11:48:19 -0400

    Add my local compiler aliases

---
+++ local/lalejini-compiler-aliases.sh
@@ -0,0 +1,17 @@
+export CC=/opt/homebrew/bin/gcc-14
+export CXX=/opt/homebrew/bin/g++-14
+export CPP=/opt/homebrew/bin/cpp-14
+export LD=/opt/homebrew/bin/gcc-14
+
+alias c++=/opt/homebrew/bin/c++-14
+alias g++=/opt/homebrew/bin/g++-14
+alias gcc=/opt/homebrew/bin/gcc-14
+alias cpp=/opt/homebrew/bin/cpp-14
+alias ld=/opt/homebrew/bin/gcc-14
+alias cc=/opt/homebrew/bin/gcc-14
+
+alias clang=/opt/homebrew/bin/gcc-14
+
+
+export CC=gcc-14 CXX=g++-14 FC=gfortran-14
+export SDKROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX14.sdk/
